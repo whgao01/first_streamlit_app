e3464cb88861c82d70792cd1b354fc0ec7e0ae56
whgao01 <105012311+whgao01@users.noreply.github.com>
2022-05-05 13:11:23 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,12 @@
-import streamlit;
+import streamlit
 
-streamlit.title('My Mom\'s New Healthy Dinner');
+streamlit.title('My Mom\'s New Healthy Dinner')
 
-streamlit.header('Breakfast Menu');
+streamlit.header('Breakfast Menu')
 
-streamlit.text('🥣 Omega 3 and Blueberry Oatmeal');
-streamlit.text('🥗 Kale, Apinach & Rocket Smoothies');
-streamlit.text('🐔 Hard-Boiled Free-Range Egg');
+streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Apinach & Rocket Smoothies')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
